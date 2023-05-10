bb8256060ab76eb0fe5a8e0df97b216d0c851066
saifulalamfahime <you@example.com>
2023-05-10 18:42:59 +0600

    Type Basics

--- output/script.js
+++ output/script.js
@@ -1,3 +1,27 @@
 "use strict";
-const Country = "You are beautiful, Archita";
-console.log(Country);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(99, 5));
+let array = ['apple', 'banana'];
+console.log(array);
+array.push("orange");
+console.log(array);
+let mixed = ["Sakib", 36, false];
+console.log(mixed);
+mixed.push(true);
+console.log(mixed);
+mixed.push(100);
+console.log(mixed);
+mixed.push({
+    playerName: "kalachan"
+});
+console.log(mixed);
+let person = {
+    name: "sakib",
+    age: 34,
+    isCaptain: true,
+};
+console.log(person);
+person.age = 100;
+console.log(person);
--- src/script.ts
+++ src/script.ts
@@ -1,2 +1,48 @@
-const Country = "You are beautiful, Archita";
-console.log(Country);
+function add(a : number, b : number) {
+    return a+b
+}
+console.log(add(99, 5));
+
+
+
+let array =['apple', 'banana'];
+
+console.log(array);
+
+array.push("orange");
+
+console.log(array)
+
+
+let mixed = ["Sakib", 36, false];
+
+console.log(mixed);
+
+mixed.push(true)
+
+console.log(mixed);
+
+mixed.push(100);
+
+console.log(mixed)
+
+mixed.push({
+    playerName: "kalachan"
+})
+
+console.log(mixed);
+
+
+let person = {
+    name: "sakib",
+    age: 34,
+    isCaptain: true,
+}
+
+console.log(person);
+
+person.age = 100;
+
+console.log(person);
+
+
